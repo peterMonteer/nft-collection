17e66d1f084fa0be238c89289ec4ce5c66d4766e
pedro <pedromalvesmonteiro@gmail.com>
2022-12-23 21:56:40 +0000

    Update metadata url, add routes

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x1d93f87356cb68aF0624dF45F39B2476BA6e3f1e";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-one-hazel.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
